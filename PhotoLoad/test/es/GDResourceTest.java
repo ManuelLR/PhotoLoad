14a0f0e4b586906a6d383dea05fd2d085252d5ec
xDeymon <daviid.tc@gmail.com>
2015-05-30 18:55:30 +0200

    No le dí a que subiese el archivo de test....

---
+++ PhotoLoad/test/es/GDResourceTest.java
@@ -0,0 +1,40 @@
+package es;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import es.server.resources.GoogleDriveResource;
+import es.shared.domain.googledrive.FileItem;
+import es.shared.domain.googledrive.Files;
+
+public class GDResourceTest {
+	
+	String token ="ya29.gwFZkXK8A2nSdbUa0Tx_hi2QmAOrzgoRxvqGRsC7m8BibdjKCnImBXwnDfCtIVJEwsQgB27DcGhxtQ";
+	String id ="";
+	
+	@Test
+	public void testGetAll(){
+		System.out.println("Prueba getAll GD");
+		GoogleDriveResource gdr = new GoogleDriveResource(token);
+		Files files = gdr.getFiles();
+		
+		assertNotNull("The collection of files is nulll", files);
+		
+		System.out.println("Listing all files:");
+		for(FileItem fi : files.getItems()){
+			System.out.println("FileItemID " + fi.getId() + " - FileName:" + fi.getOriginalFilename());
+		}
+	}
+	
+	@Test
+	public void testDownload(){
+		System.out.println("Prueba donwload GD");
+		String url;
+		GoogleDriveResource gdr = new GoogleDriveResource(token);
+		url = gdr.getFiles().getItems().get(1).getWebContentLink();
+		assertNotNull("The url is null", url);
+		System.out.println(url);
+	}
+
+}
